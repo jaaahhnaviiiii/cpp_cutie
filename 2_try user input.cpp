0725fc3e2e81603422f62b8d91b8b08ca3c9e0f8
Jahnavi Singh <jahnavisingh567@gmail.com>
2026-07-01 22:37:34 +0530

    Start my C++ journey

---
+++ 2_try user input.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std; 
+
+int main() {
+    int age;
+
+    cout<< "Enter age:";
+    cin>> age;
+
+    cout<< "You entered: " << age;
+
+    return 0;
+}
---
+++ 3_add numbers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main () 
+{   int a, b, sum;
+
+    cout << "Enter first number:";
+    cin >> a;
+    cout << "Enter second number:";
+    cin >> b;
+    sum = a+b;
+    cout << "Sum is:" << sum;
+    return 0;
+}
