c2f5dc2149cf59cc295721559aa17e19d6ee5095
Jahnavi Singh <jahnavisingh567@gmail.com>
2026-07-14 00:57:30 +0530

    padhle yaar

---
+++ 24_stepping.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int getUserInput()
+{
+    int a{};
+    cout << "Enter number: ";
+    cin >> a;
+
+    return a;
+}
+
+int multiply(int b, int c)
+{
+    return b * c;
+}
+
+int main()
+{
+    int x{getUserInput()};
+    int y{getUserInput()};
+
+    cout << "Product is: " << multiply(x, y) << '\n';
+
+    return 0;
+}
+
+/* Debugging procces:
+ctrl + shift + P
+start debugging
+g++ etc etc
+then menu comes
+STEP IN: executes line by line
+STEP OUT: whole function at once
+STEP OVER: next line executes
+variables section (left side, run and debug)
+*/
