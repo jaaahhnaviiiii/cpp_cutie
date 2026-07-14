46c8bcf0b26aa92d0b0d8b6b1b035a77a6270857
Jahnavi Singh <jahnavisingh567@gmail.com>
2026-07-14 13:53:49 +0530

    padhle yaar

---
+++ 26_watching variables.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x{1};
+    cout << x << '\n';
+
+    x = x + 2;
+    cout << x << '\n';
+
+    x = x + 3;
+    cout << x << '\n';
+
+    return 0;
+}
